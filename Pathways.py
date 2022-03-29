fddb59489192a50502d08a9319668a4dbdfa4272
GitMasterBart <bartengels15@live.nl>
2022-03-29 14:05:53 +0200

    Addtion Pathway file in form of Pathway.py v1

---
+++ Pathways.py
@@ -0,0 +1,11 @@
+import os
+
+# input and Output pathways used in StartProcesses
+virtual_envPathway = '~/Desktop/StageWetsus2022/BakedInBiobakery/'
+output_file_Location = './Demodatafiles'
+input_file_Location ="~/Desktop/StageWetsus2022/BakedInBiobakery/media/"
+
+# HtmlScripts and filePaths used in views.py
+bashscript = "HumanBashScript.sh"
+base_html = "base.html"
+HumanPage_html = "HumanNpage.html"
