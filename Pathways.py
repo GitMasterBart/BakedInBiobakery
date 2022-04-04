b5ccbe423aa92390a8d8669b829c2aec9d9f6868
GitMasterBart <bartengels15@live.nl>
2022-04-04 15:59:37 +0200

    Pathways added and changed

--- Pathways.py
+++ Pathways.py
@@ -1,11 +1,15 @@
 import os
 
 # input and Output pathways used in StartProcesses
-virtual_envPathway = '~/Desktop/StageWetsus2022/BakedInBiobakery/'
+virtual_envPathway = '~/Desktop/StageWetsus2022/BakedInBiobakery'
 output_file_Location = './Demodatafiles'
-input_file_Location ="~/Desktop/StageWetsus2022/BakedInBiobakery/media/"
+input_file_Location ="~/Desktop/StageWetsus2022/BakedInBiobakery/media/tmp/"
+input_file_Location_sh ="~/Desktop/StageWetsus2022/BakedInBiobakery/media/"
 
 # HtmlScripts and filePaths used in views.py
 bashscript = "HumanBashScript.sh"
 base_html = "base.html"
 HumanPage_html = "HumanNpage.html"
+Dashboard_html = "Dashboard.html"
+imgFasqc_folder = "/Users/bengels/Desktop/StageWetsus2022/BakedInBiobakery/static/fastqcfiles/images/"
+
