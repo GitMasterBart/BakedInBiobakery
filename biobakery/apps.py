c993911ae28e0e9500dce2abeb69278f78eee50a
GitMasterBart <bartengels15@live.nl>
2022-05-02 14:24:30 +0200

    changes back-end

--- biobakery/apps.py
+++ biobakery/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class BiobakeryConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    # default_auto_field = 'django.db.models.BigAutoField'
     name = 'biobakery'
+    # label = 'biobakery'
+    # verbose_name = "biobakery2"
+
