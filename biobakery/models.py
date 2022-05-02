29eaecf47d9f5eac213d289b8b250f8b29560119
GitMasterBart <bartengels15@live.nl>
2022-05-02 14:23:46 +0200

    modified views and models

--- biobakery/models.py
+++ biobakery/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+import os
+
+import djangoProject
+
 
 class Department(models.Model):
     name = models.CharField(max_length=15)
     department_id = models.AutoField(primary_key=True)
+
     # researches = models.ManyToManyField(Researches.researches_id,blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
 
 class Users(models.Model):
     initials = models.CharField(max_length=4)
@@ -15,6 +21,7 @@ class Users(models.Model):
 
     def __str__(self):
         return self.initials
+
 
 class Researches(models.Model):
     name = models.CharField(max_length=50)
@@ -26,6 +33,10 @@ class Researches(models.Model):
         return self.name
 
 
-
-
-
+class DumpTable(models.Model):
+    gene = models.CharField(max_length=25)
+    family = models.CharField(max_length=50)
+    sample = models.CharField(max_length=60)
+    result = models.IntegerField()
+    researches_id = models.ForeignKey(Researches, blank=True, null=True, on_delete=models.PROTECT)
+    user_id = models.ForeignKey(Users, blank=True, null=True, on_delete=models.PROTECT)
