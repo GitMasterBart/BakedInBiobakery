30ac4cd17d45fae7ba2e624045c5f3eb8534a0a3
GitMasterBart <bartengels15@live.nl>
2022-05-02 15:20:16 +0200

    changes to write to database not yes ready for use v.1

--- biobakery/appModels/write_to_database.py
+++ biobakery/appModels/write_to_database.py
@@ -27,21 +27,27 @@ class WriteToDb:
 
     def __init__(self):
         self.file = "/Users/bengels/Desktop/output_data/output_gentable.tsv"
-        self.intials = "beng"
-        self.department = "microbio"
+        self.intials = "BENG"
+        self.department = "Microbiologie"
         self.research = "intitials_datum_departmen_onderzoek"
 
     def parse_tsv_file(self):
         with open(self.file) as myfile:
             head = [next(myfile) for x in range(10)]
-        for i in range(len(head)):
-            print(head[int(i) -1][0:19])
+            for i in myfile:
+                # print(head[int(i) -1][0:19])
 
-    def add_to_db(self):
-        Researches.objects.create("")
+                print(i.split(" ")[1].split("\t"))
 
 
-        print(Users.objects.all())
+    def add_research_to_db(self):
+        user_id = Users.objects.filter(initials=self.intials).values_list('User_id', flat=True).first()
+        department_id = Department.objects.filter(name=self.department).values_list('department_id', flat=True).first()
+        Researches.objects.create(name=self.research, department_id=department_id, user_id_id=user_id)
+
+
+
+
 
 def main():
     WriteToDb().parse_tsv_file()
