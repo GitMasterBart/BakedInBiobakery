d34db1bd0db3f4e1b075369e2aaa197b8ad87dc6
GitMasterBart <bartengels15@live.nl>
2022-05-03 17:19:28 +0200

    biobakery/appModels/write_to_database.py v.1 : ready to be used

--- biobakery/appModels/write_to_database.py
+++ biobakery/appModels/write_to_database.py
@@ -1,56 +1,132 @@
 #!/env/bin/python3
 
-import mysql
-from django.conf import settings
-from mysql.connector import connect
+"""
+This class pushes the result data in a database, it uses django
+models to make contact with the mysql database. It is a class that needs five variables:
+ file, initials, department, date, research_name
+"""
 
-import django, os
+import time
+import os
+from django.conf import settings
+from django.db.utils import DataError
+import django
+import pandas as pd
+from biobakery.models import Users, Department, Researches, DumpTable
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "djangoProject.settings"
 django.setup()
 
-from django.conf import settings
 if not settings.configured:
     settings.configure(
-    # DATABASE_ENGINE = 'django.db.backends.mysql',
-    # DATABASE_NAME = 'biobakery',
-    # DATABASE_USER = 'root',
-    # DATABASE_PASSWORD = 'rakker444',
-    # DATABASE_HOST = '127.0.0.1',
-    # DATABASE_PORT = '3306',
-)
-
-from biobakery.models import *
+        # DATABASE_ENGINE = 'django.db.backends.mysql',
+        # DATABASE_NAME = 'biobakery',
+        # DATABASE_USER = 'root',
+        # DATABASE_PASSWORD = 'rakker444',
+        # DATABASE_HOST = '127.0.0.1',
+        # DATABASE_PORT = '3306',
+    )
 
 
 class WriteToDb:
+    """
+    WritTodb is a class that contains 4 classes to write data to a database,
+    this database is set in the setting from this djangoproject. further is each methode named
+    after the data that is parses. : (add_Users_to_db, add_department_to_db,
+    add_research_to_adb, add_results_to_db )
+    """
 
-    def __init__(self):
-        self.file = "/Users/bengels/Desktop/output_data/output_gentable.tsv"
-        self.intials = "BENG"
-        self.department = "Microbiologie"
-        self.research = "intitials_datum_departmen_onderzoek"
+    def __init__(self, file, initials, department, date, research_name):
+        self.file = file
+        self.initials = initials
+        self.department = department
+        self.research = research_name + "_" + date + "_" + initials
 
-    def parse_tsv_file(self):
-        with open(self.file) as myfile:
-            head = [next(myfile) for x in range(10)]
-            for i in myfile:
-                # print(head[int(i) -1][0:19])
+        self.user_id = Users.objects.filter(initials=self.initials) \
+            .values_list('User_id', flat=True).first()
+        self.department_id = Department.objects.filter(name=self.department) \
+            .values_list('department_id', flat=True).first()
+        self.research_id = Researches.objects.filter(name=self.research) \
+            .values_list('researches_id', flat=True).first()
 
-                print(i.split(" ")[1].split("\t"))
+    def add_users_to_db(self):
+        """
+        Adds a user to database if it does not already exists.
+        :return if DataError : str with "Datapoint already exists."
+        """
+        try:
+            if not Users.objects.filter(initials=self.initials).exists():
+                Users.objects.create(initials=self.initials)
+        except DataError:
+            return "Datapoint already exists."
 
+    def add_department_to_db(self):
+        """
+        Adds a department to database if it does not already exists.
+        :return: if DataError: str("Datapoint already exists.")
+        """
+        try:
+            if not Department.objects.filter(name=self.department).exists():
+                Department.objects.create(name=self.department)
+        except DataError:
+            return "Datapoint already exists."
 
     def add_research_to_db(self):
-        user_id = Users.objects.filter(initials=self.intials).values_list('User_id', flat=True).first()
-        department_id = Department.objects.filter(name=self.department).values_list('department_id', flat=True).first()
-        Researches.objects.create(name=self.research, department_id=department_id, user_id_id=user_id)
+        """
+        Adds research to database if it does not already exists.
+        :return: if DataError: str("Datapoint already exists.")
+        """
+        try:
+            if not Researches.objects.filter(name=self.research, department_id=self.department_id,
+                                             user_id_id=self.user_id).exists():
+                Researches.objects.create(name=self.research, department_id=self.department_id,
+                                          user_id_id=self.user_id)
+                self.research_id = Researches.objects.filter(name=self.research) \
+                    .values_list('researches_id', flat=True).first()
+        except DataError:
+            return "Datapoint already exists."
 
+    def add_results_to_db(self):
+        """
+        Adds all te result with the samples to a database if they doe not already exists.
+        :return: if DataError: str("Datapoints already exists.")
+        """
 
+        try:
+            transformed_table = pd.DataFrame(pd.melt(pd.read_table(self.file, lineterminator='\n'),
+                                                     id_vars="# Gene Family"))
+            for i in range(transformed_table.size):
+                gene = transformed_table.values[i][0].split(" ")[0][0:19]
+                if len(transformed_table.values[i][0].split(' ')) == 2:
+                    family = transformed_table.values[i][0].split(' ')[1]
+                else:
+                    family = '\t'
+                sample = str(transformed_table.values[i][1])
+                result = transformed_table.values[i][2]
 
+                if not DumpTable.objects.filter(gene=gene, family=family, sample=sample,
+                                                result=result, researches_id_id=self.research_id,
+                                                user_id_id=self.user_id).exists():
+                    DumpTable.objects.create(gene=gene, family=family, sample=sample, result=result,
+                                             researches_id_id=self.research_id,
+                                             user_id_id=self.user_id)
+        except DataError:
+            return "Datapoints already exists."
 
 
 def main():
-    WriteToDb().parse_tsv_file()
+    start = time.process_time()
+    createdb = WriteToDb("/Users/bengels/Desktop/output_data/output_gentable.tsv",
+                         "beng", "Microbiologie",
+                         "2022-05-03", "FirstTestOnderzoek")
+    createdb.add_users_to_db()
+    createdb.add_research_to_db()
+    createdb.add_results_to_db()
+    stop = time.process_time()
+    with open("/Users/bengels/Desktop/output_data/new.txt", "w") as new_file:
+        new_file.write("Elapsed time during the whole program in seconds: {} "
+                       .format(str(stop - start)))
+
 
 if __name__ == '__main__':
     main()
