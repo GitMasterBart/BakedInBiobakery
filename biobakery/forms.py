2558a070506e5bc01ceb0123390a70e559c35c12
GitMasterBart <bartengels15@live.nl>
2022-05-03 17:24:19 +0200

    biobakery/forms.py new form added for setting intitials v1.1

--- biobakery/forms.py
+++ biobakery/forms.py
@@ -6,6 +6,9 @@ class DateInput(forms.DateInput):
 class ApplicatonForm(forms.Form):
     CHOICES = [('human', 'HUMAnN3'), ('phyloplan', 'PhyloPhlAn3') ,('metaplan', 'metaphlan3')]
     BiobakeryTool = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
-    project = forms.CharField()
+    project = forms.CharField(max_length=15)
     date = forms.DateField(widget=DateInput)
     input_file = forms.FileField()
+
+class NewUserForm(forms.Form):
+    initials = forms.CharField(max_length=4)
