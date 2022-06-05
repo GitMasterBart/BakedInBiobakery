e17cfb2c1c48b857d2d99068d05bfaa68c6a6c50
GitMasterBart <bartengels15@live.nl>
2022-06-05 13:34:01 +0200

    changes static files

---
+++ Error_messages.py
@@ -0,0 +1,4 @@
+FILENOTFOUNDERROR = "Your file naming is not as it soud be. read the instructions and try again."
+WRONGEXTENTIONERROR = "Your file does not have te right EXTENTION"
+FIlESINZIPNOTCORRECT = "YOU NEED TE CHANGE YOUR FILE STRUCTURE TO MAKE THE APPLICATON WORK. " \
+                       "GO TO THE INFORMATION PAGE FOR MORE INFORMATION"
--- Pathways.py
+++ Pathways.py
@@ -1,25 +1,24 @@
-import os
 
 # input and Output pathways used in StartProcesses
-virtual_envPathway = '~/Desktop/StageWetsus2022/BakedInBiobakery/'
-output_file_Location = '~/Desktop/output_data/'
-input_file_Location = "/Users/bengels/Desktop/Uploaded_files/"
+VIRTUALENVPATHWAY = '~/Desktop/StageWetsus2022/BakedInBiobakery/'
+OUTPUTFILELOCATION = '~/Desktop/output_data/'
+INPUTFILESLOCATION = "/Users/bengels/Desktop/Uploaded_files/"
 # virtual_envPathwayfastqc ="~/Desktop/StageWetsus2022/BakedInBiobakery/media/fastqc/"
 # input_file_Location_sh ="~/Desktop/StageWetsus2022/BakedInBiobakery/media/"
 
 # HtmlScripts and filePaths used in views.py
-BashScriptHuman = "/Users/bengels/Desktop/StageWetsus2022/BakedInBiobakery/static/sh_scripts/HumanBashStarter.sh"
-UniprotDatabase = "~/Desktop/Uploaded_files/humann_dbs/uniref"
+LOCATIONBASHSCRIPTHUMAN = "/Users/bengels/Desktop/StageWetsus2022/BakedInBiobakery/static/sh_scripts/HumanBashStarter.sh"
+LOCATIONUNIPROTDATABASE = "~/Desktop/Uploaded_files/humann_dbs/uniref"
 
 
-imgFasqc_folder = "/Users/bengels/Desktop/StageWetsus2022/BakedInBiobakery/static/fastqcfiles/images_after/"
+FASTQCIMGFOLDER = "/Users/bengels/Desktop/StageWetsus2022/BakedInBiobakery/static/fastqcfiles/images_after/"
 
 # uploaded filepath
 
-uploaded_filepath = "/Users/bengels/Desktop/Uploaded_files/"
+LOCATIONUPLOADEDFILES = "/Users/bengels/Desktop/Uploaded_files/"
 
 #databases
-unireffull = "/Users/bengels/Desktop/Uploaded_files/humann_dbs/unireffull/"
-chocophlan = "/Users/bengels/Desktop/Uploaded_files/humann_dbs/chocophlan"
-slivadatabase = "/Users/bengels/Desktop/Uploaded_files/humann_dbs/silvadb"
-trimmomatic = "~/Desktop/kneaddataMap/Trimmomatic-0.36/"
+LOCATIONUNIREFFPULLDATABASE = "/Users/bengels/Desktop/Uploaded_files/humann_dbs/unireffull/"
+LOCATIONCHOCOPHLANDATABASE = "/Users/bengels/Desktop/Uploaded_files/humann_dbs/chocophlan"
+LOCATIONSILVADATABASE = "/Users/bengels/Desktop/Uploaded_files/humann_dbs/silvadb"
+LOCATIONTRIMMOMATICDATABASE = "~/Desktop/kneaddataMap/Trimmomatic-0.36/"
