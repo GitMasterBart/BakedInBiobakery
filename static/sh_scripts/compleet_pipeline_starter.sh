f7701c2cbd2dc50522e81b4b61a185617e2df22c
GitMasterBart <bartengels15@live.nl>
2022-06-06 20:39:35 +0200

    new sh scripts

--- static/sh_scripts/compleet_pipeline_starter.sh
+++ static/sh_scripts/compleet_pipeline_starter.sh
@@ -4,7 +4,7 @@ source ~/Desktop/PipelineEnv/bin/activate
 
 cd /Users/bengels/Desktop/StageWetsus2022/BakedInBiobakery/biobakery/appModels/SnakePipeMultiHumaNn/humantool_snakefile/
 
-snakemake --cores 2 --jobs 200 --config inputfiles=$1 name=$2 user_index=$3 research_index=$4 dataset=$5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} &
+snakemake --cores 2 --jobs 200 --config inputfiles=$1 name=$2 user_index=$3 research_index=$4 dataset=$5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} --rerun-incomplete &
 
 #snakemake --cores 2 --jobs 20 --config inputfiles=$1 dataset="$2" $3 $4 $5 $6 $7 $8 $9 ${11} ${12} ${13} --unlock &
 
