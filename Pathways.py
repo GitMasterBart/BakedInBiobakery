5966f45fa6324b63a095ee6e12511d65c4e44ae6
GitMasterBart <bartengels15@live.nl>
2022-07-11 11:58:45 +0200

    patway file without patways

--- Pathways.py
+++ Pathways.py
@@ -1,28 +1,26 @@
 
 # input and Output pathways used in StartProcesses
-LOCATIONOFYOURPROJECT = '/Users/bengels/Desktop/StageWetsus2022/BakedInBiobakery/'
-OUTPUTFILELOCATION = '~/Desktop/output_data/'
-INPUTFILESLOCATION = "/Users/bengels/Desktop/Uploaded_files/"
+LOCATIONOFYOURPROJECT =
+OUTPUTFILELOCATION =
+INPUTFILESLOCATION =
 
 
-# virtual_envPathwayfastqc ="~/Desktop/StageWetsus2022/BakedInBiobakery/media/fastqc/"
-# input_file_Location_sh ="~/Desktop/StageWetsus2022/BakedInBiobakery/media/"
 
 # HtmlScripts and filePaths used in views.py
-LOCATIONBASHSCRIPTHUMAN = "/Users/bengels/Desktop/StageWetsus2022/BakedInBiobakery/static/sh_scripts/HumanBashStarter.sh"
-LOCATIONBASHSCRIPTKNEADDATA = "/Users/bengels/Desktop/StageWetsus2022/BakedInBiobakery/static/sh_scripts/kneaddata_bash_script.sh"
-LOCATIONBASHSCRIPTCOMPLEETPIPELINE = "/Users/bengels/Desktop/StageWetsus2022/BakedInBiobakery/static/sh_scripts/compleet_pipeline_starter.sh"
-LOCATIONUNIPROTDATABASE = "~/Desktop/Uploaded_files/humann_dbs/uniref"
+LOCATIONBASHSCRIPTHUMAN =
+LOCATIONBASHSCRIPTKNEADDATA =
+LOCATIONBASHSCRIPTCOMPLEETPIPELINE =
+LOCATIONUNIPROTDATABASE =
 
 
-FASTQCIMGFOLDER = "/Users/bengels/Desktop/StageWetsus2022/BakedInBiobakery/static/img/fastqc_results"
+FASTQCIMGFOLDER =
 
 # uploaded filepath
 
-LOCATIONUPLOADEDFILES = "/Users/bengels/Desktop/Uploaded_files/"
+LOCATIONUPLOADEDFILES =
 
 #databases
-LOCATIONUNIREFFPULLDATABASE = "/Users/bengels/Desktop/Uploaded_files/humann_dbs/unireffull/"
-LOCATIONCHOCOPHLANDATABASE = "/Users/bengels/Desktop/Uploaded_files/humann_dbs/chocophlan"
-LOCATIONSILVADATABASE = "/Users/bengels/Desktop/Uploaded_files/humann_dbs/silvadb"
-LOCATIONTRIMMOMATICDATABASE = "/Users/bengels/Desktop/Uploaded_files/humann_dbs/Trimmomatic-0.36/"
+LOCATIONUNIREFFPULLDATABASE =
+LOCATIONCHOCOPHLANDATABASE =
+LOCATIONSILVADATABASE =
+LOCATIONTRIMMOMATICDATABASE =
