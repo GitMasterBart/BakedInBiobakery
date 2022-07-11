527052bc6bfcd071328162f78e3fa3f6257208d3
GitMasterBart <bartengels15@live.nl>
2022-07-11 11:57:40 +0200

    error messages v1.2

--- Error_messages.py
+++ Error_messages.py
@@ -1,5 +1,5 @@
 FILENOTFOUNDERROR = "Your file naming is not as it soud be. read the instructions and try again."
-WRONGEXTENTIONERROR = "Your file does not have te right EXTENTION"
+WRONGEXTENTIONERROR = "Your file does not have te right EXTENTION or is a wrongly zipped"
 FIlESINZIPNOTCORRECT = "YOU NEED TE CHANGE YOUR FILE STRUCTURE TO MAKE THE APPLICATON WORK. " \
                        "GO TO THE INFORMATION PAGE FOR MORE INFORMATION"
 FILENOTFASTQERROR = "gz files doe not have fastq as extention change this and try again"
