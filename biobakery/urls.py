388fd6b6ad6c186fb1eb258e2842a706c7d1b4df
GitMasterBart <bartengels15@live.nl>
2022-07-26 09:21:53 +0200

    adding author information

--- biobakery/urls.py
+++ biobakery/urls.py
@@ -1,16 +1,22 @@
-from urllib import request
+#!/env/bin/python3
+"""
+This file looks add the requests and redirects it to the
+correct view class
+"""
+
+__author__ = "Bart Engels"
+__date__ = "28-07-2022"
+__version__ = "v1"
 
 from django.urls import path
 
 # from polls.views import Myviews
 from biobakery.views import *
-from . import views
 
 urlpatterns = [
-    # path('', Myviews.geeks_view, name='base'),
     path('Home', HomeViews.as_view()),
     path('Upload', UploadViews.as_view()),
-    path('succes', SuccesVieuws.as_view()),
+    path('succes', SuccesViews.as_view()),
     path('addUser', AddUser.as_view()),
     path('information', Information.as_view()),
     path('fastqcCheck', FastqcCheckViews.as_view()),
