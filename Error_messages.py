ed4f4cc60153e6bc04ed89f86cb7d74b574cd205
GitMasterBart <bartengels15@live.nl>
2022-07-26 09:36:08 +0200

    authors information added

--- Error_messages.py
+++ Error_messages.py
@@ -1,3 +1,11 @@
+"""
+consists of error messages for often occurring errors
+"""
+
+__author__ = "Bart Engels"
+__date__ = "28-07-2022"
+__version__ = "v1"
+
 FILENOTFOUNDERROR = "Your file naming is not as it soud be. read the instructions and try again."
 WRONGEXTENTIONERROR = "Your file does not have te right EXTENTION or is a wrongly zipped"
 FIlESINZIPNOTCORRECT = "YOU NEED TE CHANGE YOUR FILE STRUCTURE TO MAKE THE APPLICATON WORK. " \
