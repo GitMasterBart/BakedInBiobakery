4909aa1f1ada96b54faafbbb39bd799f7f07bcf1
GitMasterBart <bartengels15@live.nl>
2022-07-26 09:25:04 +0200

    add author information biobakery/apps.py

--- biobakery/apps.py
+++ biobakery/apps.py
@@ -1,9 +1,14 @@
+#!/env/bin/python3
+
+"""
+config file biobakery
+"""
+
+__author__ = "Bart Engels"
+__date__ = "28-07-2022"
+__version__ = "v1"
+
 from django.apps import AppConfig
 
-
 class BiobakeryConfig(AppConfig):
-    # default_auto_field = 'django.db.models.BigAutoField'
     name = 'biobakery'
-    # label = 'biobakery'
-    # verbose_name = "biobakery2"
-
